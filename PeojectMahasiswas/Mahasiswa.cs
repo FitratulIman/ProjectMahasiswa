60f5d50f2b8f74b22ad6bcba9d66fe5306ad7af3
FitratulIman <fitratul.2811@students.amikom.ac.id>
2020-07-08 12:28:04 +0700

    ResponsiPemrograman

---
+++ PeojectMahasiswas/Mahasiswa.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ProjectPenjualan
+{
+    public class Mahasiswa
+    {
+        internal string nama;
+
+        public int Nim { get; set; }
+        public string Nama { get; set; }
+        public string Jenis { get; set; }
+        public int IPK { get; set; }
+
+    }
+}
